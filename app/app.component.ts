a1289b601ae586765a123649abb13dd039384e7d
Michael H. Oshita <ijinpublic+github@gmail.com>
2016-02-05 17:14:51 +0900

    select hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,17 +12,20 @@ interface Hero {
         <h1>{{title}}</h1>
         <h2>My Heroes</h2>
         <ul class="heroes">
-          <li *ngFor="#hero of heroes">
+          <li *ngFor="#hero of heroes"
+            [class.selected]="hero === selectedHero"
+            (click)="onSelect(hero)"
+          >
             <!-- each hero goes here -->
             <span class="badge">{{hero.id}}</span> {{hero.name}}
           </li>
         </ul>
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label>name: </label>
+        <div *ngIf="selectedHero">
+            <h2>{{selectedHero.name}} details!</h2>
+            <div><label>id: </label>{{selectedHero.id}}</div>
             <div>
-                <input [(ngModel)]="hero.name" placeholder="name">
+                <label>name: </label>
+                <input [(ngModel)]="selectedHero.name" placeholder="name">
             </div>
         </div>
         `
@@ -78,11 +81,10 @@ interface Hero {
 
 export class AppComponent {
     public title = 'Tour of Heroes';
-    public hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    public selectedHero: Hero;
     public heroes = HEROES;
+
+    onSelect(hero: Hero) {this.selectedHero = hero; }
 }
 
 
