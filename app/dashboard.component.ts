d77be41e9acf0906f52b797f8c291f17ff4fc3d9
Michael H. Oshita <ijinpublic+github@gmail.com>
2016-02-10 00:04:11 +0900

    display dashboard contents

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,8 +1,21 @@
 import {Component} from 'angular2/core';
+import {OnInit} from 'angular2/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-dashboard',
-    template: '<h3>My Dashboard</h3>'
+    templateUrl: 'app/dashboard.component.html'
 })
 export class DashboardComponent {
+    heroes: Hero[] = [];
+
+    constructor(private _heroService: HeroService) { }
+
+    ngOnInit() {
+        this._heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(1,5));
+    }
+
+    gotoDetail(){ /* not implemented yet */}
 }
